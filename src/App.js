f9ea513e5d743fd7e834fe1d6dcf5f13dc61cdec
Son Hyejoo <seongwoo.cho@gmail.com>
2023-03-31 18:59:14 +0000

    created Button component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log("clicked");
+  }
+
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
